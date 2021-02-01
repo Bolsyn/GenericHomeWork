859644ed75ab1e04a6f4516b304b6cbd8c587236
Bolsyn <Serikagis@yandex.ru>
2021-02-01 21:28:50 +0600

    Add project files.

---
+++ GenericHomeWork/Karta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericHomeWork
+{
+    public class Karta
+    {
+        public Suit Suits { get; set; }
+        public TypeOfCard Type { get; set; }
+        public static bool operator >=(Karta a, Karta b) 
+        {
+            return (a.Suits >= b.Suits);
+        }
+        public static bool operator <=(Karta a, Karta b)
+        {
+            return (a.Suits <= b.Suits);
+        }
+    }
+}
---
+++ GenericHomeWork/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericHomeWork
+{
+    public class Player
+    {
+        public List<Karta> Hand { get; set; }
+        Random rand = new Random();
+        public Karta Output()
+        {
+            return Hand[rand.Next() % Hand.Count];
+        }
+    }
+}
